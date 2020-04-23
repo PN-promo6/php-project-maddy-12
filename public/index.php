7d19115a7d495294be37b6f74aa407db6a09d479
Madina <benaferi-madina@hotmail.fr>
2020-04-23 11:22:58 +0200

    V-1

--- public/index.php
+++ public/index.php
@@ -60,10 +60,18 @@
                     <img src="https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">
+
+                            <ul>
+                                <li>Artist : </li>
+                                <li>Description : </li>
+                                <li>Tarifs : </li>
+                                <li>Type : </li>
+                            </ul>
+                        </p>
                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
-                    <button type="button" class="btn btn-dark">Dark</button>
+                    <button type="button" class="btn btn-dark">Contact me</button>
                 </div>
                 <div class="card mb-4 mr-3">
                     <img src="https://images.unsplash.com/photo-1523554888454-84137e72c3ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" class="card-img-top" alt="...">
@@ -164,7 +172,6 @@
                 </div>
             </div>
         </div>
-
     </div>
     <!-- Contenu -->
 
